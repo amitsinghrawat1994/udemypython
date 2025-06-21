22db5297c7647e0a12b6ccd8e6d286e6f344705e
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2025-06-21 14:25:31 +0530

    42. Creating a "while" Loop

---
+++ test.py
@@ -0,0 +1,10 @@
+var1 = 1
+var2 = 2    
+
+# add two numbers and print the result
+result = var1 + var2
+print('The sum of {0} and {1} is {2}'.format(var1, var2, result))
+
+
+blockchain = [1, 8.6, 5.1]
+
